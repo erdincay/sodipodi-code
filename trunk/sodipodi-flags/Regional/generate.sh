7f7946eec7d6d31594f53a8fd95fa2151a5d0eff
theuraeus <theuraeus@2490c5c0-b34c-4913-a0bd-69599dd9f015>
2003-10-29 18:01:00 +0000

    Latest batch of flags :)

--- trunk/sodipodi-flags/Regional/generate.sh
+++ trunk/sodipodi-flags/Regional/generate.sh
@@ -1,1 +1,1 @@
-for i in  alaska_USA colorado_USA galicia_spain jutland_denmark; do sodipodi --export-png=${i}.png --export-background=white --export-width=128 ${i}.svg ; done
+for i in  northcarolina_USA; do sodipodi --export-png=${i}.png --export-background=white --export-width=128 ${i}.svg ; done
