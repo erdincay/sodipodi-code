305a7dab1b05adbb043c2c3e75fe809d668e464e
suwalski <suwalski@2490c5c0-b34c-4913-a0bd-69599dd9f015>
2003-11-01 08:20:24 +0000

    Adding clipart section.

---
+++ trunk/sodipodi-web/clipart/clipart.php
@@ -0,0 +1,81 @@
+<?php
+global $PHP_SELF;
+global $n;
+$cols = 4;
+$perpage = 24;
+
+// Set page variables
+if ($n == 0) $n = 1;
+$area = $pagedata['area'];
+
+$count = 0;
+$dir = opendir("./clipart/$area/");
+while ($file = readdir($dir)) {
+	if (ereg(".txt$", $file)) {
+		$filelist[] = $file;
+	}
+}
+closedir($dir);
+
+sort($filelist);
+$total = count($filelist);
+
+print "<CENTER><FORM ACTION='$PHP_SELF' METHOD='POST'>\n";
+print "<INPUT TYPE='hidden' NAME='section' VALUE='$section/$area'>\n";
+print "<INPUT TYPE='hidden' NAME='area' VALUE='$area'>\n";
+print "<SELECT NAME='n'>\n";
+for ($i=0; $i<($total / $perpage); $i++) {
+	$def = '';
+	$first = ($i * $perpage) + 1;
+	$last = ($i + 1) * $perpage;
+	if ($last > $total) $last = $total;
+
+	if ($first == $n) $def = ' selected';
+
+	print "<OPTION VALUE='$first'$def>$first - $last</OPTION>\n";
+}
+print "</SELECT><INPUT TYPE='SUBMIT' VALUE='Go'></FORM></CENTER>\n";
+
+print "<BR><BR><TABLE WIDTH='100%' BORDER='0'>\n";
+
+for ($i=($n-1); $i<($n + $perpage - 1); $i++) {
+	$file = $filelist[$i];
+	if ($i >= $total) break;
+
+	if ($count % $cols == 0) print "<TR ALIGN='center' VALIGN='bottom'>\n";
+	
+	$eval = file("./clipart/$area/$file");
+	eval(join('', $eval));
+
+	$basefile = basename($file, '.txt');
+
+	print "<TD>\n";
+	print "<A HREF='clipart/$area/$basefile.svg'>\n";
+	print "<IMG SRC='clipart/$area/$basefile.png' BORDER='1'><BR>\n";
+	print "<B>$title</B></A><BR>By: $author<BR>\n";
+	print "<BR>&nbsp;</TD>\n";
+
+	$count++;
+	if ($count % $cols == 0) print "</TR>\n";
+}
+
+// Make sure table closed off properly
+if ($count % $cols != 0) print "</TR>\n";
+?>
+
+</TABLE>
+
+<BR><BR>
+<CENTER>
+
+<?php
+$left = $n - $perpage;
+if ($left > 0) print "<A HREF='$PHP_SELF?section=$section/$area&area=$area&n=$left'><<< Previous |</A>\n";
+
+$right = $n + $perpage;
+if ($right < $total) print "<A HREF='$PHP_SELF?section=$section/$area&area=$area&n=$right'>| Next >>>></A>\n";
+?>
+
+<BR><BR>Total: <B><?php echo $total?></B>
+
+</CENTER>
---
+++ trunk/sodipodi-web/clipart/clipartintro.php
@@ -0,0 +1,43 @@
+<P>
+Welcome to the Sodipodi <B>Clipart Page</B>.
+</P>
+
+<P>
+Over time, this section will become populated with free SVG drawings that can be downloaded and used freely in your designs.
+</P>
+
+<P>
+Presently, an actively growing flag section is available.
+</P>
+
+<p>
+<B>Regarding Flags:</B><BR>
+SVG versions of flags from different countries from around the world are
+provided for public use.  Note that these may not be authoritatively
+correct (colors, dimensions, etc.) but if you know the correct RGB
+triplets, feel free to send a corrected version to the Sodipodi web
+administors.  Also see 
+<a href="http://flagspot.net/">http://flagspot.net/</a> for details
+about flags around the world and 
+<a href="http://www.ngw.nl/">http://www.ngw.nl/</a> for civic heraldry.
+</p>
+
+<p>
+If submitting a flag, please scale document to correct aspect ratio,
+with shorter edge being 140mm, and make flag to cover the whole
+page area. This makes generating bitmaps automatically much easier.
+</p>
+
+<p>
+Official dimentions are preferred. 
+If the official dimensions of flag have other aspect ratio than
+3:2, it is a good idea to submit both official and 3:2 versions. The
+latter will be useful in situations, where all flags have to be
+identically shaped (for example list of buttons for different languages
+in user interface).
+</p>
+
+<p>
+These images may be freely reused, limited only by the normal
+regulations of the nation in question.
+</p>
