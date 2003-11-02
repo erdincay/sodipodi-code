2982cdec2a24631ed397eefa5306ded67d4a6ffe
theuraeus <theuraeus@2490c5c0-b34c-4913-a0bd-69599dd9f015>
2003-11-02 15:01:25 +0000

    latest updates

--- trunk/sodipodi-flags/Regional/generate.sh
+++ trunk/sodipodi-flags/Regional/generate.sh
@@ -1,1 +1,1 @@
-for i in  england_gibraltar; do sodipodi --export-png=${i}.png --export-background=white --export-width=128 ${i}.svg ; done
+for i in  usa_northcarolina usa_southcarolina usa_alabama; do sodipodi --export-png=${i}.png --export-background=white --export-width=128 ${i}.svg ; done
