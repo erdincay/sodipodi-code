6d725fb97ca03f0be1b46f86502919cf0bf0ff0c
theuraeus <theuraeus@2490c5c0-b34c-4913-a0bd-69599dd9f015>
2003-11-03 20:37:07 +0000

    latest flags

--- sodipodi-flags/generate.sh
+++ sodipodi-flags/generate.sh
@@ -1,1 +1,1 @@
-for i in ontario_arms; do sodipodi --export-png=${i}.png --export-background=white --export-width=128 ${i}.svg ; done
+for i in spain_madrid; do sodipodi --export-png=${i}.png --export-background=white --export-width=128 ${i}.svg ; done
