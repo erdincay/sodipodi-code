05139b716e8e8aa43a896e39001fa3014159c767
theuraeus <theuraeus@2490c5c0-b34c-4913-a0bd-69599dd9f015>
2003-11-13 19:14:17 +0000

    add latest flags

---
+++ sodipodi-flags/create_flagtarball.sh
@@ -0,0 +1,4 @@
+# This script should be run one directory lower than the sodipodi-flags CVS
+# directory
+tar -cvf sodipodi-flags-1.0.tar sodipodi-flags/nations/*.svg sodipodi-flags/Regional/*.svg sodipodi-flags/Organizations sodipodi-flags/historic/*.svg sodipodi-flags/license/*.* sodipodi-flags/3x2/*.svg sodipodi-flags/4x3/*.svg sodipodi-flags/COPYING sodipodi-flags/README sodipodi-flags/CREDITS sodipodi-flags/generate.sh
+zip sodipodi-flags-1.0.zip sodipodi-flags/nations/*.svg sodipodi-flags/Regional/*.svg sodipodi-flags/Organizations sodipodi-flags/historic/*.svg sodipodi-flags/license/*.* sodipodi-flags/3x2/*.svg sodipodi-flags/4x3/*.svg sodipodi-flags/COPYING sodipodi-flags/README sodipodi-flags/CREDITS sodipodi-flags/generate.sh
