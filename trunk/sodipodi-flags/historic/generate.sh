e970ff006fdb8fe1b3580895f8faf95cb87a2ba4
theuraeus <theuraeus@2490c5c0-b34c-4913-a0bd-69599dd9f015>
2003-11-20 22:24:55 +0000

    bahamas is recoginized as independent

--- trunk/sodipodi-flags/historic/generate.sh
+++ trunk/sodipodi-flags/historic/generate.sh
@@ -1,1 +1,1 @@
-for i in germany_east germany_swastika USSR; do sodipodi --export-png=${i}.png --export-background=white --export-width=128 ${i}.svg ; done
+for i in belarus_historic; do sodipodi --export-png=${i}.png --export-background=white --export-width=128 ${i}.svg ; done
