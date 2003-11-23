ac508a1b1381fb0401aa2e61aecaa336a602b011
theuraeus <theuraeus@2490c5c0-b34c-4913-a0bd-69599dd9f015>
2003-11-23 22:04:05 +0000

    fix tobias name

--- trunk/sodipodi-flags/historic/generate.sh
+++ trunk/sodipodi-flags/historic/generate.sh
@@ -1,1 +1,1 @@
-for i in belarus_historic; do sodipodi --export-png=${i}.png --export-background=white --export-width=128 ${i}.svg ; done
+for i in georgia_stgeorge; do sodipodi --export-png=${i}.png --export-background=white --export-width=128 ${i}.svg ; done
