79db681b11f1de41688e33a3519532f96ae024a4
bakeyev <bakeyev@2490c5c0-b34c-4913-a0bd-69599dd9f015>
2003-12-20 22:05:57 +0000

    Release version bumped to 1.5.
    Added call to gen_cc.pl before packing to get country code list in the release.
    List of the files moved into the variable, to make it consistent. Files gen_cc.pl
    and list are now excluded from the release. Fixed problem of not including
    Organizations/ into the zip archive.

--- sodipodi-flags/create_flagtarball.sh
+++ sodipodi-flags/create_flagtarball.sh
@@ -1,4 +1,22 @@
 # This script should be run one directory lower than the sodipodi-flags CVS
 # directory
-tar -cvf sodipodi-flags-1.4.tar sodipodi-flags/nations/*.svg sodipodi-flags/Regional/*.svg sodipodi-flags/Organizations sodipodi-flags/historic/*.svg sodipodi-flags/license/*.* sodipodi-flags/3x2/*.svg sodipodi-flags/4x3/*.svg sodipodi-flags/COPYING sodipodi-flags/README sodipodi-flags/CREDITS sodipodi-flags/generate.sh sodipodi-flags/gen_cc.pl sodipodi-flags/list
-zip sodipodi-flags-1.4.zip sodipodi-flags/nations/*.svg sodipodi-flags/Regional/*.svg sodipodi-flags/Organizations sodipodi-flags/historic/*.svg sodipodi-flags/license/*.* sodipodi-flags/3x2/*.svg sodipodi-flags/4x3/*.svg sodipodi-flags/COPYING sodipodi-flags/README sodipodi-flags/CREDITS sodipodi-flags/generate.sh sodipodi-flags/gen_cc.pl sodipodi-flags/list
+RELEASE=sodipodi-flags-1.5
+FILES="sodipodi-flags/cc/*.svg sodipodi-flags/nations/*.svg \
+    sodipodi-flags/Regional/*.svg sodipodi-flags/Organizations/*.svg \
+    sodipodi-flags/historic/*.svg sodipodi-flags/license/*.* \
+    sodipodi-flags/3x2/*.svg sodipodi-flags/4x3/*.svg \
+    sodipodi-flags/COPYING sodipodi-flags/README \
+    sodipodi-flags/CREDITS sodipodi-flags/generate.sh"
+
+if [ -d sodipodi-flags/cc ]; then
+    rm -fr sodipodi-flags/cc
+fi
+
+if [ -f sodipodi-flags/gen_cc.pl ]; then
+    cd sodipodi-flags 
+    perl ./gen_cc.pl
+    cd ..
+fi
+
+tar -cvf ${RELEASE}.tar ${FILES}
+zip ${RELEASE}.zip ${FILES}
