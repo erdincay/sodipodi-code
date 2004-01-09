745f00af23daa91fcd36e1f13d2452af6ac1deab
theuraeus <theuraeus@2490c5c0-b34c-4913-a0bd-69599dd9f015>
2004-01-09 20:48:28 +0000

    add latest flags

--- trunk/sodipodi-flags/generate.sh
+++ trunk/sodipodi-flags/generate.sh
@@ -1,1 +1,1 @@
-for i in ; do sodipodi --export-png=${i}.png --export-background=white --export-width=128 ${i}.svg ; done
+for i in banana_republic ; do sodipodi --export-png=${i}.png --export-background=white --export-width=128 ${i}.svg ; done
