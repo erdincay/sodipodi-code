0aa43a25871074e8bd858d269db38f9ebd7ad29b
theuraeus <theuraeus@2490c5c0-b34c-4913-a0bd-69599dd9f015>
2004-01-30 21:49:31 +0000

    adding all the latest flags and some I had forgotten to cvs commit to this module

--- trunk/sodipodi-flags/Regional/generate.sh
+++ trunk/sodipodi-flags/Regional/generate.sh
@@ -1,1 +1,1 @@
-for i in basque; do sodipodi --export-png=${i}.png --export-background=white --export-width=128 ${i}.svg ; done
+for i in france_normandy; do sodipodi --export-png=${i}.png --export-background=white --export-width=128 ${i}.svg ; done
