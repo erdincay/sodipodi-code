400972effadc51911cc3e6756a426a4da8dc139c
theuraeus <theuraeus@2490c5c0-b34c-4913-a0bd-69599dd9f015>
2005-03-31 10:33:04 +0000

    notice of move of flag collection

--- trunk/sodipodi-web/clipart/clipartintro.php
+++ trunk/sodipodi-web/clipart/clipartintro.php
@@ -12,35 +12,6 @@ Presently, an actively growing flag section is available.
 
 <p>
 <B>Regarding Flags:</B><BR>
-SVG versions of flags from different countries from around the world are
-provided for public use.  Note that these may not be authoritatively
-correct (colors, dimensions, etc.) but if you know the correct RGB
-triplets, feel free to send a corrected version to the Sodipodi web
-administors.  Also see 
-<a href="http://flagspot.net/">http://flagspot.net/</a> for details
-about flags around the world and 
-<a href="http://www.ngw.nl/">http://www.ngw.nl/</a> for civic heraldry.
-</p>
-
-<p>
-If submitting a flag, please scale document to correct aspect ratio,
-with shorter edge being 140mm, and make flag to cover the whole
-page area. This makes generating bitmaps automatically much easier.
-</p>
-<P>
-All contributed clipart should be dedicated to the <A HREF="http://creativecommons.org/licenses/publicdomain/">public domain</A>. Please follow the <A HREF="http://creativecommons.org/license/publicdomain-2">Creative Commons process</A> to dedicate the clipart to the public domain. Be sure to send in the documents the Creative Commons process generates for you together with the clipart.
-</P>
-<p>
-Official dimentions are preferred. 
-If the official dimensions of flag have other aspect ratio than
-3:2, it is a good idea to submit both official and 3:2 versions. The
-latter will be useful in situations, where all flags have to be
-identically shaped (for example list of buttons for different languages
-in user interface).
-</p>
-<P>Packaged releases of the Sodipodi flag collection can be downloaded from our <A HREF="http://prdownloads.sourceforge.net/sodipodi/">Sourceforge download site</A>. The package is available in both .tar.gz and zip formats. 
-<p>
-These images may be freely reused, limited only by the normal
-regulations of the nation in question.
+The Flag collection is now hosted at <a href="http://www.openclipart.org/cgi-bin/navigate/signs_and_symbols/flags">openclipart.org</a>. The collection there is much more complete than the one hosted here. The flags here are kept here now for archieving purposes and to not break old links.
 </p>
 <A HREF="http://creativecommons.org/licenses/publicdomain/"><IMG SRC="http://www.sodipodi.com/clipart/pd.gif"></A>
