7ce872fecefff45ea536beb1518269e229a73879
lauris <lauris@2490c5c0-b34c-4913-a0bd-69599dd9f015>
2009-03-26 23:07:09 +0000

    Made pixblock internals public

--- trunk/libnr/pixblock.h
+++ trunk/libnr/pixblock.h
@@ -19,82 +19,83 @@
 
 namespace NR {
 
-class PixBlock {
+struct PixBlock {
 public:
 	enum Mode { G8, R8G8B8, R8G8B8A8N, R8G8B8A8P, NUM_MODES };
 private:
-	NRPixBlock _pb;
 	// Not copyable
 	PixBlock& operator= (const PixBlock& pixblock) { assert (false); return *this; }
 public:
+	NRPixBlock pb;
+
 	// Constructor
-	PixBlock (void) { nr_pixblock_setup_fast (&_pb, NR_PIXBLOCK_MODE_G8, 0, 0, 0, 0, 0); }
-	PixBlock (PixBlock& pb) {
-		nr_pixblock_setup_extern (&_pb, pb._pb.mode, pb._pb.area.x0, pb._pb.area.y0, pb._pb.area.x1, pb._pb.area.y1, NR_PIXBLOCK_PX (&pb._pb), pb._pb.rs, pb._pb.empty, 0);
+	PixBlock (void) { nr_pixblock_setup_fast (&pb, NR_PIXBLOCK_MODE_G8, 0, 0, 0, 0, 0); }
+	PixBlock (PixBlock& ppb) {
+		nr_pixblock_setup_extern (&pb, ppb.pb.mode, ppb.pb.area.x0, ppb.pb.area.y0, ppb.pb.area.x1, ppb.pb.area.y1, NR_PIXBLOCK_PX (&ppb.pb), ppb.pb.rs, ppb.pb.empty, 0);
 	}
 	PixBlock (Mode mode, int x0, int y0, int x1, int y1, bool fast, bool clear) {
 		if (fast) {
-			nr_pixblock_setup_fast (&_pb, mode, x0, y0, x1, y1, clear);
+			nr_pixblock_setup_fast (&pb, mode, x0, y0, x1, y1, clear);
 		} else {
-			nr_pixblock_setup (&_pb, mode, x0, y0, x1, y1, clear);
+			nr_pixblock_setup (&pb, mode, x0, y0, x1, y1, clear);
 		}
 	}
 	PixBlock (Mode mode, int x0, int y0, int x1, int y1, unsigned char *px, int rs, bool empty, bool clear) {
-		nr_pixblock_setup_extern (&_pb, mode, x0, y0, x1, y1, px, rs, empty, clear);
+		nr_pixblock_setup_extern (&pb, mode, x0, y0, x1, y1, px, rs, empty, clear);
 	}
 	// Destructor
-	~PixBlock (void) { nr_pixblock_release (&_pb); }
+	~PixBlock (void) { nr_pixblock_release (&pb); }
 	// Initialization
 	void clear (void) {
-		nr_pixblock_release (&_pb);
-		nr_pixblock_setup_fast (&_pb, G8, 0, 0, 0, 0, 0);
+		nr_pixblock_release (&pb);
+		nr_pixblock_setup_fast (&pb, G8, 0, 0, 0, 0, 0);
 	}
 	void set (Mode mode, int x0, int y0, int x1, int y1, bool fast, bool clear) {
-		nr_pixblock_release (&_pb);
+		nr_pixblock_release (&pb);
 		if (fast) {
-			nr_pixblock_setup_fast (&_pb, mode, x0, y0, x1, y1, clear);
+			nr_pixblock_setup_fast (&pb, mode, x0, y0, x1, y1, clear);
 		} else {
-			nr_pixblock_setup (&_pb, mode, x0, y0, x1, y1, clear);
+			nr_pixblock_setup (&pb, mode, x0, y0, x1, y1, clear);
 		}
 	}
 	void set (Mode mode, int x0, int y0, int x1, int y1, unsigned char *px, int rs, bool empty, bool clear) {
-		nr_pixblock_release (&_pb);
-		nr_pixblock_setup_extern (&_pb, mode, x0, y0, x1, y1, px, rs, empty, clear);
+		nr_pixblock_release (&pb);
+		nr_pixblock_setup_extern (&pb, mode, x0, y0, x1, y1, px, rs, empty, clear);
 	}
-	void set (PixBlock& pb) {
-		nr_pixblock_release (&_pb);
-		nr_pixblock_setup_extern (&_pb, pb._pb.mode, pb._pb.area.x0, pb._pb.area.y0, pb._pb.area.x1, pb._pb.area.y1, NR_PIXBLOCK_PX (&pb._pb), pb._pb.rs, pb._pb.empty, 0);
+	void set (PixBlock& ppb) {
+		nr_pixblock_release (&pb);
+		nr_pixblock_setup_extern (&pb, ppb.pb.mode, ppb.pb.area.x0, ppb.pb.area.y0, ppb.pb.area.x1, ppb.pb.area.y1, NR_PIXBLOCK_PX (&ppb.pb), ppb.pb.rs, ppb.pb.empty, 0);
 	}
 	// Access
-	bool isZero (void) const { return (_pb.area.x0 >= _pb.area.x1) || (_pb.area.y0 >= _pb.area.y1); }
-	unsigned char *getPixels (void) { return NR_PIXBLOCK_PX (&_pb); }
-	const unsigned char *getPixels (void) const { return NR_PIXBLOCK_PX (&_pb); }
-	Mode getMode (void) const { return (Mode) _pb.mode; }
-	int getBPP (void) const { return NR_PIXBLOCK_BPP (&_pb); }
-	bool isEmpty (void) const { return _pb.empty == 1; }
-	int getRowStride (void) const { return (int) _pb.rs; }
-	int getWidth (void) const { return _pb.area.x1 - _pb.area.x0; }
-	int getHeight (void) const { return _pb.area.y1 - _pb.area.y0; }
-	int getX0 (void) const { return _pb.area.x0; }
-	int getX1 (void) const { return _pb.area.x1; }
-	int getY0 (void) const { return _pb.area.y0; }
-	int getY1 (void) const { return _pb.area.y1; }
-	bool hasAlpha (void) const { return nr_pixblock_has_alpha (&_pb) != 0; }
+	bool isZero (void) const { return (pb.area.x0 >= pb.area.x1) || (pb.area.y0 >= pb.area.y1); }
+	unsigned char *getPixels (void) { return NR_PIXBLOCK_PX (&pb); }
+	const unsigned char *getPixels (void) const { return NR_PIXBLOCK_PX (&pb); }
+	Mode getMode (void) const { return (Mode) pb.mode; }
+	int getBPP (void) const { return NR_PIXBLOCK_BPP (&pb); }
+	bool isEmpty (void) const { return pb.empty == 1; }
+	int getRowStride (void) const { return (int) pb.rs; }
+	int getWidth (void) const { return pb.area.x1 - pb.area.x0; }
+	int getHeight (void) const { return pb.area.y1 - pb.area.y0; }
+	int getX0 (void) const { return pb.area.x0; }
+	int getX1 (void) const { return pb.area.x1; }
+	int getY0 (void) const { return pb.area.y0; }
+	int getY1 (void) const { return pb.area.y1; }
+	bool hasAlpha (void) const { return nr_pixblock_has_alpha (&pb) != 0; }
 	// Mark pixblock as nonempty
-	void markDirty (void) { _pb.empty = 0; }
+	void markDirty (void) { pb.empty = 0; }
 	// Get row pointer
-	unsigned char *getRow (int row) { return  NR_PIXBLOCK_PX (&_pb) + row * _pb.rs; }
-	const unsigned char *getRow (int row) const { return  NR_PIXBLOCK_PX (&_pb) + row * _pb.rs; }
+	unsigned char *getRow (int row) { return  NR_PIXBLOCK_PX (&pb) + row * pb.rs; }
+	const unsigned char *getRow (int row) const { return  NR_PIXBLOCK_PX (&pb) + row * pb.rs; }
 	// Get pixel pointer
-	unsigned char *getPixel (int row, int col) { return NR_PIXBLOCK_PX (&_pb) + row * _pb.rs + col * NR_PIXBLOCK_BPP (&_pb); }
-	const unsigned char *getPixel (int row, int col) const { return NR_PIXBLOCK_PX (&_pb) + row * _pb.rs + col * NR_PIXBLOCK_BPP (&_pb); }
+	unsigned char *getPixel (int row, int col) { return NR_PIXBLOCK_PX (&pb) + row * pb.rs + col * NR_PIXBLOCK_BPP (&pb); }
+	const unsigned char *getPixel (int row, int col) const { return NR_PIXBLOCK_PX (&pb) + row * pb.rs + col * NR_PIXBLOCK_BPP (&pb); }
 	// Get minimal and maximal values
-	void getChannelLimits (unsigned int minv[], unsigned int maxv[]) const { nr_pixblock_get_channel_limits (&_pb, minv, maxv); }
+	void getChannelLimits (unsigned int minv[], unsigned int maxv[]) const { nr_pixblock_get_channel_limits (&pb, minv, maxv); }
 	// Drawing
-	void fill (unsigned int rgba32) { nr_pixblock_fill (&_pb, rgba32); }
+	void fill (unsigned int rgba32) { nr_pixblock_fill (&pb, rgba32); }
 	// Blitting
-	void blit (const PixBlock& source, unsigned int alpha) { nr_blit_pixblock_pixblock_alpha (&_pb, &source._pb, alpha); if (alpha && !source._pb.empty) _pb.empty = 0; }
-	void drawMask (const PixBlock& mask, unsigned int rgba32) { nr_blit_pixblock_mask_rgba32 (&_pb, &mask._pb, rgba32); if (rgba32 & 255) _pb.empty = 0; }
+	void blit (const PixBlock& source, unsigned int alpha) { nr_blit_pixblock_pixblock_alpha (&pb, &source.pb, alpha); if (alpha && !source.pb.empty) pb.empty = 0; }
+	void drawMask (const PixBlock& mask, unsigned int rgba32) { nr_blit_pixblock_mask_rgba32 (&pb, &mask.pb, rgba32); if (rgba32 & 255) pb.empty = 0; }
 };
 
 } // Namespace NR
