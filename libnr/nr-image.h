dc8042d2461ac8dca56e6388e6cf1e9806454522
lauris <lauris@2490c5c0-b34c-4913-a0bd-69599dd9f015>
2010-04-30 21:11:45 +0000

    Added NRImage

---
+++ libnr/nr-image.h
@@ -0,0 +1,46 @@
+#ifndef __NR_IMAGE_H__
+#define __NR_IMAGE_H__
+
+/*
+ * Pixel buffer rendering library
+ *
+ * Authors:
+ *   Lauris Kaplinski <lauris@kaplinski.com>
+ *
+ * This code is in public domain
+ */
+
+typedef struct _NRImage NRImage;
+
+#include <libnr/nr-pixblock.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * NRImage
+ *
+ * An pixel buffer with reference counting
+ *
+ */
+
+struct _NRImage {
+	unsigned int refcount;
+
+	NRPixBlock pixels;
+};
+
+NRImage *nr_image_new (void);
+void nr_image_ref (NRImage *image);
+void nr_image_unref (NRImage *image);
+
+/* Copy on write semantics implementation */
+/* It is allowed to call it with NULL image */
+NRImage *nr_image_ensure_private_empty (NRImage *image);
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif
